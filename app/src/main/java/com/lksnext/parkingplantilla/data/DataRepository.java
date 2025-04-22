1e784b6130b56d0205a3d95203c6c01645331092
AitorSarasola <asarasola028@ikasle.ehu.eus>
2025-04-22 15:19:20 +0200

    Start

---
+++ app/src/main/java/com/lksnext/parkingplantilla/data/DataRepository.java
@@ -0,0 +1,38 @@
+package com.lksnext.parkingplantilla.data;
+
+import com.lksnext.parkingplantilla.domain.Callback;
+
+public class DataRepository {
+
+    private static DataRepository instance;
+    private DataRepository(){
+
+    }
+
+    //Creación de la instancia en caso de que no exista.
+    public static synchronized DataRepository getInstance(){
+        if (instance==null){
+            instance = new DataRepository();
+        }
+        return instance;
+    }
+
+    //Petición del login.
+    public String login(String email, String pass, Callback callback){
+        try {
+            //Realizar petición
+            if (email.isEmpty() && pass.isEmpty()){
+                throw new IllegalArgumentException("*The email and the password can not be empty");
+            }else if (email.isEmpty()){
+                throw new IllegalArgumentException("*The email can not be empty");
+            }else if(pass.isEmpty()){
+                throw new IllegalArgumentException("*The password can not be empty");
+            }
+            callback.onSuccess();
+            return "";
+        } catch (Exception e){
+            callback.onFailure();
+            return e.getMessage();
+        }
+    }
+}
