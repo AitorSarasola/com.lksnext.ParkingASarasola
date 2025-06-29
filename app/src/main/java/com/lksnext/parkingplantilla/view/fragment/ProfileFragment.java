83ad8a6da6363666bfed04a5fde30f823a719bcf
AitorSarasola <asarasola028@ikasle.ehu.eus>
2025-06-29 21:17:15 +0200

    Errors Fixed

--- app/src/main/java/com/lksnext/parkingplantilla/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/lksnext/parkingplantilla/view/fragment/ProfileFragment.java
@@ -7,43 +7,38 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import java.util.ArrayList;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.lksnext.parkingplantilla.R;
-import com.lksnext.parkingplantilla.databinding.ActivityLoginBinding;
 import com.lksnext.parkingplantilla.databinding.FragmentProfileBinding;
 import com.lksnext.parkingplantilla.domain.Car;
 import com.lksnext.parkingplantilla.domain.CarAdapter;
 import com.lksnext.parkingplantilla.view.activity.LoginActivity;
-import com.lksnext.parkingplantilla.view.activity.MainActivity;
-import com.google.firebase.auth.GoogleAuthProvider;
 
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import java.util.ArrayList;
 
 public class ProfileFragment extends Fragment {
 
+    private FragmentProfileBinding binding;
     private RecyclerView recyclerView;
     private CarAdapter cocheAdapter;
     private ArrayList<Car> listaCoches;
-    private FragmentProfileBinding binding;
 
     public ProfileFragment() {
-        // Es necesario un constructor vacio
+        // Constructor vacío necesario
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Asignar la vista (layout) al fragmento
-
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        recyclerView = root.findViewById(R.id.recyclerViewCoches);
+        recyclerView = binding.recyclerViewCoches;
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        listaCoches = new ArrayList<Car>();
+        listaCoches = new ArrayList<>();
         listaCoches.add(new Car("1234ABC", Car.Type.Coche, Car.Label.B, false));
         listaCoches.add(new Car("5678DEF", Car.Type.Moto, Car.Label.ECO, false));
         listaCoches.add(new Car("9999XYZ", Car.Type.Coche_Para_Discapacitados, Car.Label.Cero_Emisiones, true));
@@ -51,17 +46,13 @@ public class ProfileFragment extends Fragment {
         cocheAdapter = new CarAdapter(listaCoches);
         recyclerView.setAdapter(cocheAdapter);
 
-        binding.logoutButton.setOnClickListener(v->{
+        binding.logoutButton.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
-
             Intent intent = new Intent(getActivity(), LoginActivity.class);
             startActivity(intent);
             getActivity().finish();
         });
 
         return root;
-
-        //return inflater.inflate(R.layout.fragment_profile, container, false);
     }
-
 }
