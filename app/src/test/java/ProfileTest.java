c5617bd4a98794046d7e40ed80c3e92ed50b2e03
AitorSarasola <asarasola028@ikasle.ehu.eus>
2025-07-06 23:57:00 +0200

    Pruebas Unitarias

---
+++ app/src/test/java/ProfileTest.java
@@ -0,0 +1,31 @@
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.lksnext.parkingplantilla.viewmodel.ProfileViewModel;
+
+public class ProfileTest {
+    @Rule
+    public InstantTaskExecutorRule instantExecutorRule = new InstantTaskExecutorRule();
+
+    @Test
+    public void testLiveDataValue() throws InterruptedException {
+        // 1. Crear instancia de tu ViewModel
+        ProfileViewModel vm = new ProfileViewModel();
+
+
+        Boolean result = LiveDataTestUtil.getValue(vm.getLogout());
+
+        assertEquals(result,Boolean.FALSE);
+
+        //No se puede hacer pq logout llama a un Handler
+        /*vm.logout();
+
+        result = LiveDataTestUtil.getValue(vm.getLogout());
+
+        assertEquals(result,Boolean.TRUE);*/
+
+
+    }
+}
